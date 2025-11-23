66f85029a27ff79799f7f328fba5871aa2a4a703
podono <podono.clach@gmail.com>
2025-11-23 11:12:40 +0900

    +-ボタンを追加 それに伴いCSSで見た目を整えた

--- cart.js
+++ cart.js
@@ -16,6 +16,21 @@ function addToCart(id) {
     renderCart();
 }
 
+// 数量を増やす
+function incrase(index) {
+    cart[index].quantity++;
+    renderCart();
+}
+
+// 数量を減らす
+function decrease(index) {
+    cart[index].quantity--;
+    if (cart[index].quantity <= 0) {
+        cart.splice(index, 1);
+    }
+    renderCart();
+}
+
 // カート表示
 function renderCart() {
     const list = document.getElementById("cart-list");
@@ -25,12 +40,27 @@ function renderCart() {
 
     cart.forEach(item => {
         const li = document.createElement("li");
-        const divNameQty = document.createElement("div");
-        divNameQty.textContent = `${item.name} × ${item.quantity}`;
+        const divName = document.createElement("div");
+        divName.textContent = `${item.name} × `;
+        const divQuntity = document.createElement("div");
+        divQuntity.className = "quantity-controls";
+        const btnIncrase = document.createElement("button");
+        btnIncrase.textContent = "+";
+        btnIncrase.onclick = () => incrase(cart.indexOf(item));
+        const btnDecrease = document.createElement("button");
+        btnDecrease.textContent = "-";
+        btnDecrease.onclick = () => decrease(cart.indexOf(item));
+        const spanQuantity = document.createElement("span");
+        spanQuantity.textContent = item.quantity;
+        divQuntity.appendChild(btnDecrease);
+        divQuntity.appendChild(spanQuantity);
+        divQuntity.appendChild(btnIncrase);
+
         const divPrice = document.createElement("div");
         divPrice.className = "price";
-        divPrice.textContent = `${item.price * item.quantity}円`;
-        li.appendChild(divNameQty);
+        li.appendChild(divName);
+        li.appendChild(divQuntity);
+        divPrice.textContent = (item.price * item.quantity) + "円";
         li.appendChild(divPrice);
 
         total += item.price * item.quantity;
